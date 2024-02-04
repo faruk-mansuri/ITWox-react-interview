43dafb317590bab913114fa25607a0d357ec8e4f
faruq <faruqmansuri707@gmail.com>
2024-02-04 18:13:43 +0530

    added gradient to landing page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
   };
 
   return (
-    <div className='h-screen p-2'>
+    <div className='h-screen p-2 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 via-cyan-500 to-blue-600'>
       <nav className='h-12 px-2 flex items-center justify-end gap-2'>
         <Link href={user ? '/dashboard' : 'signin'}>
           <Button variant='outlined'>
@@ -33,7 +33,7 @@ const Hero = () => {
       </nav>
 
       <div className='h-[calc(100%-3rem)] flex flex-col justify-center items-center'>
-        <h1 className='text-9xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-500'>
+        <h1 className='text-9xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-red-500'>
           ITWox
         </h1>
         <p className='text-xl'>
